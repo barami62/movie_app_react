7201a3731d6ab010eb58de4d0695153a8f25be0f
unknown <wodbs7758@naver.com>
2020-07-18 16:49:15 +0900

    save

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+function Food ({name, picture}) {
+  return (
+    <div>
+      <h1>I like {name}!</h1>
+      <img src={picture} />
+    </div>);
+}
+
+const foodILike = [
+  {
+    name: 'kimchi',
+    image: 'https://search.naver.com/search.naver?where=image&sm=tab_jum&query=%EA%B9%80%EC%B9%98#'
+  },
+  {
+    name: 'ramen',
+    image: 'https://search.naver.com/search.naver?sm=tab_hty.top&where=image&query=%EB%9D%BC%EB%A9%B4&oquery=%EA%B9%80%EC%B9%98&tqi=Ux6Etwp0YidssZ1wVWNssssstJK-155485#'
+  },
+  {
+    name: 'bulgogi',
+    image: 'https://search.naver.com/search.naver?sm=tab_hty.top&where=image&query=%EB%B6%88%EA%B3%A0%EA%B8%B0&oquery=%EB%9D%BC%EB%A9%B4&tqi=Ux6EOsp0YidssZE9LVwssssstJN-211923#'
+  }
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return (<div>
+    {foodILike.map(dish => <Food name={dish.name} picture={dish.image}/>)}
     </div>
   );
 }
