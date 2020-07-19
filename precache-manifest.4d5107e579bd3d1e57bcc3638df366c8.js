74231e9ea7d90593ca4ab86f1509e8b0a9afdf05
unknown <wodbs7758@naver.com>
2020-07-19 12:57:57 +0900

    Updates

---
+++ precache-manifest.4d5107e579bd3d1e57bcc3638df366c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6128ef52608bcc8893dbb613d0906bcd",
+    "url": "/barami62/movie_app_react/index.html"
+  },
+  {
+    "revision": "4e922827fbd0f2f8cf03",
+    "url": "/barami62/movie_app_react/static/css/main.ade52640.chunk.css"
+  },
+  {
+    "revision": "118469322d9ff5a4cfd2",
+    "url": "/barami62/movie_app_react/static/js/2.a786e030.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/barami62/movie_app_react/static/js/2.a786e030.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e922827fbd0f2f8cf03",
+    "url": "/barami62/movie_app_react/static/js/main.f5f616c1.chunk.js"
+  },
+  {
+    "revision": "9fdb730db20138d3fa12",
+    "url": "/barami62/movie_app_react/static/js/runtime-main.011accee.js"
+  }
+]);
